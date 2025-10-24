ff6000c6ef3ebce8713ccfa63aa9e46a52a08022
Deniz <deniz@example.com>
2025-10-24 20:24:14 +0300

    chore: add CleanVersion migration (stable faz5 schema)

---
+++ StoryCollab.Infrastructure/Migrations/20251024172254_CleanVersion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoryCollab.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CleanVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
